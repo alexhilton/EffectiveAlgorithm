14f7a99d9f1b2a3cc5a609d2ec7d5f1ca4ef361b
Alex Hilton <dragonhilton@gmail.com>
2022-02-20 11:31:36 +0800

    Binary addition in string format.

---
+++ effectiveoffer/src/main/java/numbers/Binaries.java
@@ -0,0 +1,23 @@
+package numbers;
+
+public class Binaries {
+    public static String stringAddition(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0) {
+            int digitA = i >= 0 ? a.charAt(i--) - '0' : 0;
+            int digitB = j >= 0 ? b.charAt(j--) - '0' : 0;
+            int sum = digitA + digitB + carry;
+            carry = sum >= 2 ? 1 : 0;
+            sum = sum >= 2 ? sum - 2 : sum;
+            result.append(sum);
+        }
+
+        if (carry == 1) {
+            result.append("1");
+        }
+        return result.reverse().toString();
+    }
+}
---
+++ effectiveoffer/src/test/java/numbers/BinaryTest.java
@@ -0,0 +1,25 @@
+package numbers;
+
+import org.junit.jupiter.api.Test;
+
+import static numbers.Binaries.stringAddition;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BinaryTest {
+    @Test
+    public void testAddBasics() {
+        assertEquals("101", stringAddition("11", "10"));
+        assertEquals("1", stringAddition("1", "0"));
+
+        assertEquals("1001", stringAddition("1000", "1"));
+        assertEquals("10000", stringAddition("1111", "1"));
+    }
+
+    @Test
+    public void testAddBoundaries() {
+        assertEquals("0", stringAddition("0", "0"));
+
+        assertEquals("10000000000000", stringAddition("1111111111111", "1"));
+        assertEquals("10000000000001", stringAddition("10000000000000", "1"));
+    }
+}
