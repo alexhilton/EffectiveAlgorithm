db73030171727e4917f1b5d36e9136442b1a02cb
Alex Hilton <dragonhilton@gmail.com>
2022-02-25 00:49:15 +0800

    Question 7.

--- effectiveoffer/src/main/java/array/ArrayQuestions.java
+++ effectiveoffer/src/main/java/array/ArrayQuestions.java
@@ -1,6 +1,48 @@
 package array;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class ArrayQuestions {
+    // Question 7
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new LinkedList<>();
+        if (nums.length >= 3) {
+            Arrays.sort(nums);
+
+            int i = 0;
+            while (i < nums.length - 2) {
+                twoSumTuple(nums, i, result);
+                int temp = nums[i];
+                while (i < nums.length && nums[i] == temp) {
+                    i++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static void twoSumTuple(int[] nums, int i, List<List<Integer>> result) {
+        int j = i + 1;
+        int k = nums.length - 1;
+        while (j < k) {
+            if (nums[i] + nums[j] + nums[k] == 0) {
+                result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                int temp = nums[j];
+                while (nums[j] == temp && j < k) {
+                    j++;
+                }
+            } else if (nums[i] + nums[j] + nums[k] < 0) {
+                j++;
+            } else {
+                k--;
+            }
+        }
+    }
+
     // Question 6
     public static int[] twoSum(int[] numbers, int target) {
         int i = 0;
--- effectiveoffer/src/test/java/array/ArrayTest.java
+++ effectiveoffer/src/test/java/array/ArrayTest.java
@@ -2,13 +2,31 @@ package array;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static array.ArrayQuestions.threeSum;
 import static array.ArrayQuestions.twoSum;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 public class ArrayTest {
     @Test
     public void testTwoSum() {
         assertArrayEquals(new int[] {0, 1}, twoSum(new int[] {1, 2}, 3));
         assertArrayEquals(new int[] {1, 3}, twoSum(new int[] {1, 2, 4, 6, 10}, 8));
+    }
+
+    @Test
+    public void testThreeSum() {
+        // Pay attention to the order, numbers are sorted ascending.
+        // And we use double pointer from both ends, so the tuple containing
+        // Smaller item comes before the bigger one.
+        assertIterableEquals(Arrays.asList(Arrays.asList(0, 0, 0)), threeSum(new int[] {0, 0, 0}));
+        assertIterableEquals(
+                Arrays.asList(Arrays.asList(-1, 0, 1), Arrays.asList(0, 0, 0)),
+                threeSum(new int[] {0, -1, 1, 3, 0, 0})
+        );
+        assertIterableEquals(Arrays.asList(Arrays.asList(-1, -1, 2), Arrays.asList(-1, 0, 1)),
+                threeSum(new int[] {-1, 0, 1, 2, -1, -4}));
     }
 }
