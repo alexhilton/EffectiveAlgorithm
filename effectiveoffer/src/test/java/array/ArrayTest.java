a47ae7e204f24d1510ef963dcaaee6832558d392
Alex Hilton <dragonhilton@gmail.com>
2022-03-01 21:57:45 +0800

    Add more tests for Question 13.

--- effectiveoffer/src/test/java/array/ArrayTest.java
+++ effectiveoffer/src/test/java/array/ArrayTest.java
@@ -82,6 +82,8 @@ public class ArrayTest {
         int[][] a = {{3, 0, 1, 4, 2}, {5, 6, 3, 2, 1}, {1, 2, 0, 1, 5}, {4, 1, 0, 1, 7}, {1, 0, 3, 0, 5}};
         prepareSums(a);
         assertEquals(3, sumRegion(0, 0, 0, 0));
+        assertEquals(14, sumRegion(0, 0, 1, 1));
         assertEquals(8, sumRegion(2, 1, 4, 3));
+        assertEquals(58, sumRegion(0, 0, 4, 4));
     }
 }
