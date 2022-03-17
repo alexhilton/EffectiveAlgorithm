7f14117553b6ffbf4d9fa707119acb62c8e88ed3
Alex Hilton <dragonhilton@gmail.com>
2022-03-17 14:40:19 +0800

    TwoSum: binaryMap.
    Use binary search for loop, Map for finding.
    
    AC: time 0, memory normal.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/hot100/P001TwoSum.java
+++ leetcode/src/main/java/hot100/P001TwoSum.java
@@ -1,5 +1,8 @@
 package hot100;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class P001TwoSum {
     public static int[] bruteForce(int[] nums, int target) {
         for (int i = 0; i < nums.length - 1; i++) {
@@ -76,6 +79,42 @@ public class P001TwoSum {
                     return result;
                 }
             }
+        }
+
+        return result;
+    }
+
+    // Binary search for loop, use hash to find target.
+    // O(n/2) for the best case, provided Map is O(1)
+    public static int[] binaryMap(int[] nums, int target) {
+        int[] result = {0, 1};
+        if (nums.length <= 2) {
+            return result;
+        }
+
+        Map<Integer, Integer> valueToIndex = new HashMap<>();
+        for (int i = 0, j = nums.length - 1; i <= j; i++, j--) {
+            if (nums[i] + nums[j] == target) {
+                // So lucky
+                result[0] = i;
+                result[1] = j;
+                break;
+            }
+            int x = target - nums[i];
+            if (valueToIndex.containsKey(x)) {
+                result[0] = i;
+                result[1] = valueToIndex.get(x);
+                break;
+            }
+            x = target - nums[j];
+            if (valueToIndex.containsKey(x)) {
+                result[0] = j;
+                result[1] = valueToIndex.get(x);
+                break;
+            }
+
+            valueToIndex.put(nums[i], i);
+            valueToIndex.put(nums[j], j);
         }
 
         return result;
--- leetcode/src/test/java/hot100/P001TwoSumTest.java
+++ leetcode/src/test/java/hot100/P001TwoSumTest.java
@@ -2,15 +2,21 @@ package hot100;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.function.BiFunction;
 
-import static hot100.P001TwoSum.bruteForce;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class P001TwoSumTest {
     @Test
     public void testTwoSum() {
-        BiFunction<int[], Integer, int[]> action = P001TwoSum::quadruple;
+        // The result has no order
+        // So, we sort it for the sake of comparing.
+        BiFunction<int[], Integer, int[]> action = (nums, target) -> {
+            int[] result = P001TwoSum.binaryMap(nums, target);
+            Arrays.sort(result);
+            return result;
+        };
 
         assertArrayEquals(new int[] {0, 1}, action.apply(new int[] {3, 3}, 6));
         assertArrayEquals(new int[] {1, 2}, action.apply(new int[] {3, 2, 4}, 6));
