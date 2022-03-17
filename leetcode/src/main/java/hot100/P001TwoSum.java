1151083a003d3006086a921d4e01ba97c2b6e888
Alex Hilton <dragonhilton@gmail.com>
2022-03-17 12:03:08 +0800

    TwoSum: better bruteforce.
    Use binary search from both end, when finding x (x=target-[i]).
    Speed doubled.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/hot100/P001TwoSum.java
+++ leetcode/src/main/java/hot100/P001TwoSum.java
@@ -12,4 +12,29 @@ public class P001TwoSum {
 
         return new int[] {};
     }
+
+    // Binary search with double pointers from both end.
+    public static int[] betterBruteForce(int[] nums, int target) {
+        int[] result = {0, 1};
+        if (nums.length <= 2) {
+            return result;
+        }
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            result[0] = i;
+            int x = target - nums[i];
+            for (int j = i + 1, k = nums.length - 1; j <= k; j++, k--) {
+                if (nums[j] == x) {
+                    result[1] = j;
+                    return result;
+                }
+                if (nums[k] == x) {
+                    result[1] = k;
+                    return result;
+                }
+            }
+        }
+
+        return result;
+    }
 }
--- leetcode/src/test/java/hot100/P001TwoSumTest.java
+++ leetcode/src/test/java/hot100/P001TwoSumTest.java
@@ -2,14 +2,18 @@ package hot100;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.BiFunction;
+
 import static hot100.P001TwoSum.bruteForce;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class P001TwoSumTest {
     @Test
     public void testTwoSum() {
-        assertArrayEquals(new int[] {0, 1}, bruteForce(new int[] {3, 3}, 6));
-        assertArrayEquals(new int[] {1, 2}, bruteForce(new int[] {3, 2, 4}, 6));
-        assertArrayEquals(new int[] {0, 1}, bruteForce(new int[] {2, 7, 11, 15}, 9));
+        BiFunction<int[], Integer, int[]> action = P001TwoSum::betterBruteForce;
+
+        assertArrayEquals(new int[] {0, 1}, action.apply(new int[] {3, 3}, 6));
+        assertArrayEquals(new int[] {1, 2}, action.apply(new int[] {3, 2, 4}, 6));
+        assertArrayEquals(new int[] {0, 1}, action.apply(new int[] {2, 7, 11, 15}, 9));
     }
 }
