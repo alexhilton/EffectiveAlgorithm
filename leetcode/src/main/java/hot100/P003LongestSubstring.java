19f0adf6c3349fa2b5e8a709bda9c9e4ced93fd0
Alex Hilton <dragonhilton@gmail.com>
2022-03-19 09:38:41 +0800

    Longest substring no repeat: Setup.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

---
+++ leetcode/src/main/java/hot100/P003LongestSubstring.java
@@ -0,0 +1,8 @@
+package hot100;
+
+// https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+public class P003LongestSubstring {
+    public static int bruteForce(String s) {
+        return s.length();
+    }
+}
---
+++ leetcode/src/test/java/hot100/P003LongestSubstringTest.java
@@ -0,0 +1,18 @@
+package hot100;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class P003LongestSubstringTest {
+    @Test
+    public void testLongestSubstring() {
+        Function<String, Integer> action = P003LongestSubstring::bruteForce;
+        assertEquals(0, action.apply(""));
+        assertEquals(3, action.apply("abcabcbb"));
+        assertEquals(1, action.apply("bbbbb"));
+        assertEquals(3, action.apply("pwwkew"));
+    }
+}
