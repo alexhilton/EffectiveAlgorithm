71846a2704a80eeb3b9986033c68956ee2828f86
Alex Hilton <dragonhilton@gmail.com>
2022-03-19 10:08:27 +0800

    Longest substring no repeat: brute force with HashMap.
    Time: 70 ms
    Memory: 41.6 M
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/hot100/P003LongestSubstring.java
+++ leetcode/src/main/java/hot100/P003LongestSubstring.java
@@ -1,8 +1,33 @@
 package hot100;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
 public class P003LongestSubstring {
     public static int bruteForce(String s) {
-        return s.length();
+        int result = 0;
+        if (s == null || s == "") {
+            return result;
+        }
+        Map<Character, Character> substring = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            substring.clear();
+            Character left = s.charAt(i);
+            substring.put(left, left);
+            int len = 1;
+            for (int j = i + 1; j < s.length(); j++) {
+                // [i, j] is the substring
+                Character right = s.charAt(j);
+                if (substring.containsKey(right)) {
+                    break;
+                } else {
+                    len++;
+                    substring.put(right, right);
+                }
+            }
+            result = Math.max(result, len);
+        }
+        return result;
     }
 }
--- leetcode/src/test/java/hot100/P003LongestSubstringTest.java
+++ leetcode/src/test/java/hot100/P003LongestSubstringTest.java
@@ -11,6 +11,9 @@ public class P003LongestSubstringTest {
     public void testLongestSubstring() {
         Function<String, Integer> action = P003LongestSubstring::bruteForce;
         assertEquals(0, action.apply(""));
+        assertEquals(1, action.apply("a"));
+        assertEquals(2, action.apply("ab"));
+        assertEquals(3, action.apply("abc"));
         assertEquals(3, action.apply("abcabcbb"));
         assertEquals(1, action.apply("bbbbb"));
         assertEquals(3, action.apply("pwwkew"));
