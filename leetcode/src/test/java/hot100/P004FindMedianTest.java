2e0ec05675342b7993e88da3f42ec2a6ee448c07
Alex Hilton <dragonhilton@gmail.com>
2022-03-21 21:33:22 +0800

    Find median：add more tests。
    Time: 1 ms
    Memory: 42.4 M
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/test/java/hot100/P004FindMedianTest.java
+++ leetcode/src/test/java/hot100/P004FindMedianTest.java
@@ -12,7 +12,15 @@ public class P004FindMedianTest {
         BiFunction<int[], int[], Double> action = P004FindMedian::bruteForce;
 
         assertEquals(0, action.apply(new int[] {0}, new int[] {0}));
+        assertEquals(1, action.apply(new int[] {1}, new int[] {}));
+        assertEquals(1, action.apply(new int[] {}, new int[] {1}));
+
         assertEquals(2, action.apply(new int[] {1, 3}, new int[] {2}));
         assertEquals(2.5, action.apply(new int[] {1, 2}, new int[] {3, 4}));
+
+        assertEquals(2.5, action.apply(new int[] {1, 3}, new int[] {2, 4}));
+        assertEquals(5, action.apply(new int[] {1, 3, 5, 7}, new int[] {6}));
+        assertEquals(3.5, action.apply(new int[] {1, 3, 5, 7}, new int[] {2, 4}));
+        assertEquals(4.5, action.apply(new int[] {1, 3, 5, 7}, new int[] {4, 6}));
     }
 }
