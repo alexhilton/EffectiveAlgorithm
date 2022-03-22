d5c21433cb8af77b9a17097bc644aec3b2b8e6ae
Alex Hilton <dragonhilton@gmail.com>
2022-03-22 18:13:00 +0800

    Setup for question 14.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

---
+++ effectiveoffer/src/main/java/strings/DoublePointer.java
@@ -0,0 +1,14 @@
+package strings;
+
+// Section 2 of Chap 03
+// Use double pointer to solve string questions.
+public class DoublePointer {
+    // Question 14
+    // Bruteforce version.
+    public static boolean checkInclusionBruteforce(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        return false;
+    }
+}
---
+++ effectiveoffer/src/test/java/strings/StringQuestionsTests.java
@@ -0,0 +1,20 @@
+package strings;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.BiFunction;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringQuestionsTests {
+    @Test
+    public void testCheckInclusion() {
+        BiFunction<String, String, Boolean> action = DoublePointer::checkInclusionBruteforce;
+
+        assertFalse(action.apply("abcdef", "abcd"));
+
+        assertTrue(action.apply("ac", "dgcaf"));
+        assertFalse(action.apply("ab", "dgcaf"));
+    }
+}
