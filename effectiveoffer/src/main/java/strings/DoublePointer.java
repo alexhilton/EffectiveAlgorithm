3bc6d020a83b8cbfe38be7a1d3c86ae3e82f2216
Alex Hilton <dragonhilton@gmail.com>
2022-03-22 18:35:41 +0800

    Bruteforce for question 14.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- effectiveoffer/src/main/java/strings/DoublePointer.java
+++ effectiveoffer/src/main/java/strings/DoublePointer.java
@@ -5,9 +5,36 @@ package strings;
 public class DoublePointer {
     // Question 14
     // Bruteforce version.
+    // O(m*n*26)
     public static boolean checkInclusionBruteforce(String s1, String s2) {
         if (s1.length() > s2.length()) {
             return false;
+        }
+
+        byte[] map = new byte[26];
+        for (char ch : s1.toCharArray()) {
+            map[ch - 'a'] += 1;
+        }
+
+        for (int i = 0; i < s2.length(); i++) {
+            int limit = Math.min(i + s1.length(), s2.length());
+            for (int j = i; j < limit; j++) {
+                int idx = s2.charAt(j) - 'a';
+                if (map[idx] > 0) {
+                    map[idx] -= 1;
+                }
+            }
+            boolean match = false;
+            for (int k = 0; k < 26; k++) {
+                match = true;
+                if (map[k] != 0) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                return true;
+            }
         }
         return false;
     }
