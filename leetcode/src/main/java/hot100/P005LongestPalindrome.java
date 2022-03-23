e9dfa4c4b215112fce877dc4426936a5ed6e5da9
Alex Hilton <dragonhilton@gmail.com>
2022-03-23 16:53:44 +0800

    Setup for P005 longest palindrome of leet code.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

---
+++ leetcode/src/main/java/hot100/P005LongestPalindrome.java
@@ -0,0 +1,11 @@
+package hot100;
+
+public class P005LongestPalindrome {
+    public static String bruteForce(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+
+        return s;
+    }
+}
---
+++ leetcode/src/test/java/hot100/P005LongestPalindromeTest.java
@@ -0,0 +1,27 @@
+package hot100;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class P005LongestPalindromeTest {
+    private static Function<String, String> action;
+
+    @BeforeAll
+    public static void setup() {
+        action = P005LongestPalindrome::bruteForce;
+    }
+
+    @Test
+    public void testBasics() {
+        assertEquals("", action.apply(""));
+
+        assertEquals("a", action.apply("a"));
+        assertEquals("a", action.apply("ab"));
+        assertEquals("aa", action.apply("aa"));
+        assertEquals("aa", action.apply("aab"));
+    }
+}
