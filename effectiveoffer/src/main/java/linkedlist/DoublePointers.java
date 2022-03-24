0fbe9cbd0fdb929930eff27f8e4dd3f8390ca047
Alex Hilton <dragonhilton@gmail.com>
2022-03-24 14:20:15 +0800

    Question 21 in the offer: evict null head from testcases.
    List must have one item at least.
    Because 1 <= k <= N. Null head (empty list) is not a legit testcase.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- effectiveoffer/src/main/java/linkedlist/DoublePointers.java
+++ effectiveoffer/src/main/java/linkedlist/DoublePointers.java
@@ -5,9 +5,6 @@ public class DoublePointers {
     // head has N nodes.
     // 1 <= k <= N
     public static ListNode deleteRearKth(ListNode head, int k) {
-        if (head == null) {
-            return null;
-        }
         ListNode dummy = new ListNode(0, head);
         ListNode left = dummy, right = dummy.next;
         for (int i = 0; i < k; i++) {
