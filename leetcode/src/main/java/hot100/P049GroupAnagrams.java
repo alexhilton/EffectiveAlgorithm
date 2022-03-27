8d3df92b68dd52fd9d03009c1556a870478157a6
Alex Hilton <dragonhilton@gmail.com>
2022-03-28 01:01:02 +0800

    P049: setup again..
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/hot100/P049GroupAnagrams.java
+++ leetcode/src/main/java/hot100/P049GroupAnagrams.java
@@ -1,10 +1,26 @@
 package hot100;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class P049GroupAnagrams {
     public static List<List<String>> groupAnagrams(String[] strs) {
-        return Arrays.asList(Arrays.asList(strs));
+        List<List<String>> result = new ArrayList<>();
+        for (String item : strs) {
+            for (List<String> subList : result) {
+
+            }
+        }
+        return result;
+    }
+
+    private static byte[] strMap(String str) {
+        byte[] map = new byte[26];
+        for (char ch : str.toCharArray()) {
+            map[ch - 'a']++;
+        }
+
+        return map;
     }
 }
