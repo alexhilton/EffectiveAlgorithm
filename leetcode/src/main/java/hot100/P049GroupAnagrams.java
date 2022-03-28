3cc5bfd19c50682f0d8afe789057b8d8a68079f7
Alex Hilton <dragonhilton@gmail.com>
2022-03-28 23:27:42 +0800

    P049: solution.
    Time: 2728 ms
    Memory: 44.3 M
    Need to optimize.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/hot100/P049GroupAnagrams.java
+++ leetcode/src/main/java/hot100/P049GroupAnagrams.java
@@ -1,16 +1,25 @@
 package hot100;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class P049GroupAnagrams {
     public static List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> result = new ArrayList<>();
-        for (String item : strs) {
+        for (int i = 0; i < strs.length; i++) {
+            byte[] current = strMap(strs[i]);
+            List<String> group = null;
             for (List<String> subList : result) {
-
+                if (match(current, strMap(subList.get(0)))) {
+                    group = subList;
+                    break;
+                }
             }
+            if (group == null) {
+                group = new ArrayList<>();
+                result.add(group);
+            }
+            group.add(strs[i]);
         }
         return result;
     }
@@ -22,5 +31,15 @@ public class P049GroupAnagrams {
         }
 
         return map;
+    }
+
+    private static boolean match(byte[] map1, byte[] map2) {
+        for (int i = 0; i < 26; i++) {
+            if (map1[i] != map2[i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- leetcode/src/test/java/hot100/P049GroupAnagramsTest.java
+++ leetcode/src/test/java/hot100/P049GroupAnagramsTest.java
@@ -3,10 +3,13 @@ package hot100;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class P049GroupAnagramsTest {
     Function<String[], String[][]> action;
@@ -15,8 +18,10 @@ public class P049GroupAnagramsTest {
     public void setup() {
         action = strs -> {
             List<List<String>> result = P049GroupAnagrams.groupAnagrams(strs);
+            Collections.sort(result, Comparator.comparingInt(List::size));
             String[][] arrays = new String[result.size()][];
             for (int i = 0; i < result.size(); i++) {
+                Collections.sort(result.get(i));
                 arrays[i] = result.get(i).toArray(new String[0]);
             }
             return arrays;
@@ -25,10 +30,10 @@ public class P049GroupAnagramsTest {
 
     @Test
     public void testGroupAnagrams() {
-        assertArrayEquals(new String[][] {{}}, action.apply(new String[] {}));
+        assertTrue(Arrays.deepEquals(new String[] {}, action.apply(new String[] {})));
 
-        assertArrayEquals(new String[][] {{"a"}}, action.apply(new String[] {"a"}));
+        assertTrue(Arrays.deepEquals(new String[][] {{"a"}}, action.apply(new String[] {"a"})));
 
-        assertArrayEquals(new String[][] {{"bat"}, {"nat","tan"}, {"ate","eat","tea"}}, action.apply(new String[] {"eat", "tea", "tan", "ate", "nat", "bat"}));
+        assertTrue(Arrays.deepEquals(new String[][] {{"bat"}, {"nat","tan"}, {"ate","eat","tea"}}, action.apply(new String[] {"eat", "tea", "tan", "ate", "nat", "bat"})));
     }
 }
