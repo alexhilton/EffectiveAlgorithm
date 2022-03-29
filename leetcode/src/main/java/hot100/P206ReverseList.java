ebe7e06eb01ee26836988153c2edfe359dbd5d03
Alex Hilton <dragonhilton@gmail.com>
2022-03-29 16:04:17 +0800

    Better version of reverse with dummy head.
    When create the dummy head, should set its next to null. This would avoid set old head next to null.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/hot100/P206ReverseList.java
+++ leetcode/src/main/java/hot100/P206ReverseList.java
@@ -4,16 +4,13 @@ import linkedlist.ListNode;
 
 public class P206ReverseList {
     public static ListNode reverseList(ListNode head) {
-        ListNode dummy = new ListNode(0, head);
-        ListNode current = dummy.next;
+        ListNode dummy = new ListNode(0);
+        ListNode current = head;
         while (current != null) {
             ListNode tmp = current.next;
             current.next = dummy.next;
             dummy.next = current;
             current = tmp;
-        }
-        if (head != null) {
-            head.next = null;
         }
         return dummy.next;
     }
