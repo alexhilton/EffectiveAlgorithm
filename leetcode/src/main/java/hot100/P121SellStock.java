5f5bdfe4eab0e37b4bd21541a91de3796e751788
Alex Hilton <dragonhilton@gmail.com>
2022-04-05 16:58:50 +0800

    P121 sell stocks: Not AC hold.
    Double pointers not accepted.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/hot100/P121SellStock.java
+++ leetcode/src/main/java/hot100/P121SellStock.java
@@ -1,6 +1,23 @@
 package hot100;
 
 public class P121SellStock {
+    public static int doublePointer(int[] prices) {
+        int result = 0;
+        int i = 0;
+        for (int j = 1; j < prices.length; j++) {
+            while (i < j && ((result == 0 && prices[i] >= prices[j]))) {
+                result = Math.max(result, prices[j] - prices[i]);
+                i++;
+            }
+            if (i > 1) {
+                i--;
+            }
+            result = Math.max(result, prices[j] - prices[i]);
+        }
+
+        return result;
+    }
+
     public static int bruteForce(int[] prices) {
         int result = 0;
         for (int i = 0; i < prices.length - 1; i++) {
--- leetcode/src/test/java/hot100/P121SellStockTest.java
+++ leetcode/src/test/java/hot100/P121SellStockTest.java
@@ -12,7 +12,7 @@ public class P121SellStockTest {
 
     @BeforeEach
     public void setup() {
-        action = P121SellStock::bruteForce;
+        action = P121SellStock::doublePointer;
     }
 
     @Test
@@ -28,5 +28,11 @@ public class P121SellStockTest {
         assertEquals(1, action.apply(new int[] {1, 2, 1}));
         assertEquals(5, action.apply(new int[] {7, 1, 5, 3, 6, 4}));
         assertEquals(0, action.apply(new int[] {7, 6, 4, 3, 1}));
+
+        assertEquals(6, action.apply(new int[] {6, 1, 3, 2, 4, 7}));
+
+        assertEquals(3, action.apply(new int[] {2, 1, 4}));
+
+        assertEquals(7, action.apply(new int[] {9, 2, 4, 6, 8, 1, 8}));
     }
 }
