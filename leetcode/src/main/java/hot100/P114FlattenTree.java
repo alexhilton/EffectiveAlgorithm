b6fac173d4cdff7eef22a9332b1dd8427a0ae2ad
Alex Hilton <dragonhilton@gmail.com>
2022-04-07 15:38:06 +0800

    P114: flatten tree: WA.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/hot100/P114FlattenTree.java
+++ leetcode/src/main/java/hot100/P114FlattenTree.java
@@ -4,6 +4,47 @@ import tree.TreeNode;
 
 public class P114FlattenTree {
     public static void flattenTree(TreeNode root) {
-        //
+        if (root == null) {
+            return;
+        }
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        if (left == null && right == null) {
+            return;
+        }
+        root.left = null;
+        root.right = left;
+        TreeNode tail = root.right;
+        if (left != null) {
+            tail = doFlatten(left, tail);
+        }
+        if (right != null) {
+            doFlatten(right, tail);
+        }
+    }
+
+    private static TreeNode doFlatten(TreeNode root, TreeNode newRoot) {
+        if (root == null) {
+            return newRoot;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        newRoot.left = null;
+
+        if (left == null && right == null) {
+            newRoot.right = root;
+            return newRoot.right;
+        }
+
+        newRoot.right = root;
+        TreeNode tail = newRoot.right;
+        if (left != null) {
+            tail = doFlatten(left, tail);
+        }
+        if (right != null) {
+            tail = doFlatten(right, tail);
+        }
+        return tail;
     }
 }
