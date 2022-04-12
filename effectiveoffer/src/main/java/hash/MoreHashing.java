6ec5f479c904d72ffdb17e7b1393c4ef6b136653
Alex Hilton <dragonhilton@gmail.com>
2022-04-12 21:58:44 +0800

    Question 35.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- effectiveoffer/src/main/java/hash/MoreHashing.java
+++ effectiveoffer/src/main/java/hash/MoreHashing.java
@@ -3,6 +3,47 @@ package hash;
 import java.util.*;
 
 public class MoreHashing {
+    // Question 35
+    public int findMinDiff(List<String> timePoints) {
+        if (timePoints.size() > 1440) {
+            return 0;
+        }
+
+        boolean[] minuteFlags = new boolean[1440];
+        for (String time : timePoints) {
+            String[] t = time.split(":");
+            int min = Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
+            if (minuteFlags[min]) {
+                return 0;
+            }
+
+            minuteFlags[min] = true;
+        }
+
+        return calcDiff(minuteFlags);
+    }
+
+    private int calcDiff(boolean[] minuteFlags) {
+        int minDiff = minuteFlags.length - 1;
+        int prev = -1;
+        int first = minuteFlags.length - 1;
+        int last = -1;
+        for (int i = 0; i < minuteFlags.length; i++) {
+            if (minuteFlags[i]) {
+                if (prev > 0) {
+                    minDiff = Math.min(i - prev, minDiff);
+                }
+
+                prev = i;
+                first = Math.min(i, first);
+                last = Math.max(i, last);
+            }
+        }
+
+        minDiff = Math.min(first + minuteFlags.length - last, minDiff);
+        return minDiff;
+    }
+
     // Question 34
     public boolean isAlienSorted(String[] words, String order) {
         int[] orderArray = new int[order.length()];
