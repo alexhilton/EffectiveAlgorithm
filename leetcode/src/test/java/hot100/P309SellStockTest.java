b48105ff5bd6334e27eb724e099f5289995aad9c
Alex Hilton <dragonhilton@gmail.com>
2022-07-25 23:41:18 +0800

    P309 setup for P309.
    More basic tests.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/test/java/hot100/P309SellStockTest.java
+++ leetcode/src/test/java/hot100/P309SellStockTest.java
@@ -2,24 +2,28 @@ package hot100;
 
 import org.junit.jupiter.api.Test;
 
+import static hot100.P309SellStock.maxProfit;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class P309SellStockTest {
     @Test
     public void testBasics() {
-        assertEquals(0, P309SellStock.maxProfit(new int[] {1}));
-        assertEquals(0, P309SellStock.maxProfit(new int[] {100}));
-        assertEquals(0, P309SellStock.maxProfit(new int[] {2, 1}));
+        assertEquals(0, maxProfit(new int[] {1}));
+        assertEquals(0, maxProfit(new int[] {100}));
+        assertEquals(0, maxProfit(new int[] {2, 1}));
+        assertEquals(0, maxProfit(new int[] {1, 1}));
+        assertEquals(0, maxProfit(new int[] {1, 1, 1}));
+        assertEquals(0, maxProfit(new int[] {7, 6, 5, 4, 3, 2, 1}));
     }
 
     @Test
     public void testDemos() {
-        assertEquals(3, P309SellStock.maxProfit(new int[] {1, 2, 3, 0, 2}));
-        assertEquals(0, P309SellStock.maxProfit(new int[] {1}));
+        assertEquals(3, maxProfit(new int[] {1, 2, 3, 0, 2}));
+        assertEquals(0, maxProfit(new int[] {1}));
     }
 
     @Test
     public void testCase27() {
-        assertEquals(3, P309SellStock.maxProfit(new int[] {2, 1, 4}));
+        assertEquals(3, maxProfit(new int[] {2, 1, 4}));
     }
 }
