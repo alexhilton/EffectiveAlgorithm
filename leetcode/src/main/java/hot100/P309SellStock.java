fda4cef9a8bed2c8fbfba7995a7d6618ecdb7417
Alex Hilton <dragonhilton@gmail.com>
2022-07-27 23:46:56 +0800

    P309 setup for P309.
    AC.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/hot100/P309SellStock.java
+++ leetcode/src/main/java/hot100/P309SellStock.java
@@ -3,26 +3,19 @@ package hot100;
 public class P309SellStock {
     public static int maxProfit(int[] prices) {
         int n = prices.length;
-        if (n == 1) {
-            return 0;
-        } else if (n == 2) {
-            return Math.max(0, prices[1] - prices[0]);
-        }
-        int[] dp = new int[n];
-        dp[1] = Math.max(0, prices[1] - prices[0]);
-        int result = dp[1];
-        for (int i = 2; i < n; i++) {
-            int k = 1;
-            for (int j = k; j < i; j++) {
-                if (prices[j] < prices[k]) {
-                    k = j;
-                }
-                int p = prices[i] - prices[k];
-                dp[i] = Math.max(dp[i], p > 0 ? dp[k - 1] + p : dp[i - 1]);
-            }
-            result = Math.max(result, dp[i]);
+        int pf = -prices[0];
+        int pg = 0;
+        int ph = 0;
+
+        for (int i = 1; i < n; i++) {
+            int f = Math.max(pf, ph - prices[i]);
+            int g = pf + prices[i];
+            int h = Math.max(pg, ph);
+            pf = f;
+            pg = g;
+            ph = h;
         }
 
-        return result;
+        return Math.max(pg, ph);
     }
 }
