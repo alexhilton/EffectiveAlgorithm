db8fe85764705489e381db56568153d54ef5fe67
Alex Hilton <dragonhilton@gmail.com>
2022-08-03 16:45:10 +0800

    P621 WA at test case 19.
    WA with priority queue.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/hot100/P621TaskScheduler.java
+++ leetcode/src/main/java/hot100/P621TaskScheduler.java
@@ -1,6 +1,7 @@
 package hot100;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class P621TaskScheduler {
     public int leastInterval(char[] tasks, int n) {
@@ -14,31 +15,39 @@ public class P621TaskScheduler {
         for (int i = 0; i < tasks.length; i++) {
             counts[tasks[i] - 'A']++;
         }
-        Arrays.sort(counts);
-        System.out.println(Arrays.toString(counts));
-        if (counts[25] == 1) {
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> b - a);
+        Arrays.stream(counts)
+                .filter(a -> a > 0)
+                .forEach(a -> heap.add(a));
+
+        int pk = heap.poll();
+        if (pk == 1) {
             return tasks.length;
         }
-        int pk = counts[25];
         int spaces = n * (pk - 1);
         int m = tasks.length - pk;
         int duration = (pk - 1) * n + pk;
-        for (int i = 24; counts[i] > 0; i--) {
-            if (counts[i] == 1) {
-                if (m > 0 && m > spaces) {
-                    duration += m - spaces;
+        while (heap.size() > 0) {
+            int k = heap.poll();
+            if (k == 1) {
+                if (spaces > 0) {
+                    if (m > spaces) {
+                        duration += m - spaces;
+                    }
+                } else {
+                    duration += m;
                 }
                 break;
+            }
+            if (spaces > 0) {
+                int nk = k - (pk - 1);
+                spaces -= (pk - 1);
+                m -= (pk - 1);
+                heap.add(nk);
             } else {
-                int k;
-                if (spaces > 0) {
-                    k = counts[i] - (pk - 1);
-                    spaces -= (pk - 1);
-                } else {
-                    k = counts[i];
-                    spaces = (k - 1) * n;
-                    pk = k;
-                }
+                pk = k;
+                spaces = (k - 1) * n;
                 duration += (k - 1) * n + k;
                 m -= k;
             }
