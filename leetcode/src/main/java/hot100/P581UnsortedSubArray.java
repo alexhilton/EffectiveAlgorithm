549edbf62cf4a0d2b23392f2cc1d3c633908b5c5
Alex Hilton <dragonhilton@gmail.com>
2022-08-04 14:38:00 +0800

    P581 testcase 119 PASS.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/hot100/P581UnsortedSubArray.java
+++ leetcode/src/main/java/hot100/P581UnsortedSubArray.java
@@ -1,5 +1,7 @@
 package hot100;
 
+import java.util.Arrays;
+
 public class P581UnsortedSubArray {
     public int findUnsortedSubarray(int[] nums) {
         int n = nums.length;
@@ -26,7 +28,26 @@ public class P581UnsortedSubArray {
         if (right == 0) {
             return 0;
         }
-
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = left; i <= right; i++) {
+            if (min > nums[i]) {
+                min = nums[i];
+            }
+            if (max < nums[i]) {
+                max = nums[i];
+            }
+        }
+        int i = left - 1;
+        while (i >= 0 && nums[i] > min) {
+            i--;
+            left--;
+        }
+        int j = right + 1;
+        while (j <= n - 1 && nums[j] < max) {
+            j++;
+            right++;
+        }
         return right - left + 1;
     }
 }
