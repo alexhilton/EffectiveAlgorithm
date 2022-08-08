edf11f5c69548d413ffb447b2aa57eeb2aa95e5f
Alex Hilton <dragonhilton@gmail.com>
2022-08-08 08:41:32 +0800

    P636 AC.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/daily/P636ExclusiveTime.java
+++ leetcode/src/main/java/daily/P636ExclusiveTime.java
@@ -1,2 +1,67 @@
-package daily;public class P636ExclusiveTime {
+package daily;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class P636ExclusiveTime {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        int[] result = new int[n];
+        Stack<Func> stack = new Stack<>();
+        for (String log : logs) {
+            Func f = parse(log);
+            dump("before", f, stack, result);
+            if (stack.isEmpty() || stack.peek().id != f.id || !f.done) {
+                stack.push(f);
+            } else {
+                Func fstart = stack.pop();
+                int execTime = f.time - fstart.time + 1;
+                result[f.id] += execTime;
+                if (!stack.isEmpty()) {
+                    int parentId = stack.peek().id;
+                    result[parentId] -= execTime;
+                }
+            }
+
+            dump("after", null, stack, result);
+        }
+
+        return result;
+    }
+
+    private Func parse(String log) {
+        String[] parts = log.split(":");
+        Func f = new Func(Integer.parseInt(parts[0]), Integer.parseInt(parts[2]));
+        f.done = parts[1].equals("end");
+        return f;
+    }
+
+    private class Func {
+        int id;
+        boolean done;
+        int time;
+
+        public Func(int id, int time) {
+            this.id = id;
+            this.time = time;
+        }
+
+        @Override
+        public String toString() {
+            return "Func{" +
+                    "id=" + id +
+                    ", done=" + done +
+                    ", time=" + time +
+                    '}';
+        }
+    }
+
+    private void dump(String header, Func f, Stack<Func> stack, int[] result) {
+        System.out.println("\n-----" + header + "-------");
+        if (f != null) {
+            System.out.println("\tf " + f);
+        }
+        System.out.println("\tstack is " + stack);
+        System.out.println("\tresult is " + Arrays.toString(result));
+    }
 }
--- leetcode/src/test/java/daily/P636ExclusiveTimeTest.java
+++ leetcode/src/test/java/daily/P636ExclusiveTimeTest.java
@@ -1,2 +1,37 @@
-package daily;public class P636ExclusiveTimeTest {
+package daily;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class P636ExclusiveTimeTest {
+    private P636ExclusiveTime instance;
+
+    @BeforeEach
+    public void setup() {
+        instance = new P636ExclusiveTime();
+    }
+
+    @Test
+    public void testDemos() {
+        assertArrayEquals(new int[] {3, 4}, instance.exclusiveTime(2, List.of("0:start:0","1:start:2","1:end:5","0:end:6")));
+        assertArrayEquals(new int[] {8}, instance.exclusiveTime(1, List.of("0:start:0","0:start:2","0:end:5","0:start:6","0:end:6","0:end:7")));
+        assertArrayEquals(new int[] {7, 1}, instance.exclusiveTime(2, List.of("0:start:0","0:start:2","0:end:5","1:start:6","1:end:6","0:end:7")));
+        assertArrayEquals(new int[] {8, 1}, instance.exclusiveTime(2, List.of("0:start:0","0:start:2","0:end:5","1:start:7","1:end:7","0:end:8")));
+        assertArrayEquals(new int[] {1}, instance.exclusiveTime(1, List.of("0:start:0","0:end:0")));
+    }
+
+    @Test
+    public void testCase10() {
+        assertArrayEquals(new int[] {20,14,35,7,6,9,10,14}, instance.exclusiveTime(8, List.of(
+                "0:start:0","1:start:5","2:start:6","3:start:9","4:start:11","5:start:12","6:start:14",
+                "7:start:15","1:start:24","1:end:29","7:end:34","6:end:37","5:end:39","4:end:40","3:end:45","0:start:49",
+                "0:end:54","5:start:55","5:end:59","4:start:63","4:end:66","2:start:69","2:end:70","2:start:74","6:start:78",
+                "0:start:79","0:end:80","6:end:85","1:start:89","1:end:93","2:end:96","2:end:100","1:end:102",
+                "2:start:105","2:end:109","0:end:114"
+        )));
+    }
 }
