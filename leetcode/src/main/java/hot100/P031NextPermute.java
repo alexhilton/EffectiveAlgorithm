92c3e1aabe3e382fa255a1632438686fefb214bf
Alex Hilton <dragonhilton@gmail.com>
2022-08-09 17:06:08 +0800

    P031 AC.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/hot100/P031NextPermute.java
+++ leetcode/src/main/java/hot100/P031NextPermute.java
@@ -3,40 +3,20 @@ package hot100;
 public class P031NextPermute {
     public void nextPermutation(int[] nums) {
         final int n = nums.length;
-        if (n == 1) {
-            return;
-        }
-        if (n == 2) {
-            swap(nums, 0, 1);
-            return;
-        }
-
-        int j = n - 1;
-        while (j > 0 && nums[j - 1] >= nums[j]) {
+        int j = n - 2;
+        while (j >= 0 && nums[j] >= nums[j + 1]) {
             j--;
         }
-
-        if (j > 1) {
-            swap(nums, j - 1, j);
-        } else if (j == 1) {
-            if (nums[0] < nums[n - 1]) {
-                int tmp = nums[n - 1];
-                for (int i = n - 1; i > 0; i--) {
-                    nums[i] = nums[i - 1];
-                }
-                nums[0] = tmp;
-            } else {
-                int tmp = nums[0];
-                for (int i = 0; i < n - 1; i++) {
-                    nums[i] = nums[i + 1];
-                }
-                nums[n - 1] = tmp;
+        if (j >= 0) {
+            int k = n - 1;
+            while (k > j && nums[k] <= nums[j]) {
+                k--;
             }
-
-        } else {
-            for (int i = 0, k = n - 1; i < k; i++, k--) {
-                swap(nums, i, k);
-            }
+            swap(nums, j, k);
+        }
+        // reverse [j + 1, n - 1]
+        for (int i = j + 1, k = n - 1; i < k; i++, k--) {
+            swap(nums, i, k);
         }
     }
 
