e166bcaf7568ed2fb41aaea6b54038850d589f6b
Alex Hilton <dragonhilton@gmail.com>
2022-08-13 14:22:43 +0800

    P768 set convinced.
    Basics and demos and more passed.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/daily/P768MostChunks.java
+++ leetcode/src/main/java/daily/P768MostChunks.java
@@ -7,16 +7,31 @@ public class P768MostChunks {
         Stack<Integer> stack = new Stack<>();
         int max = arr[0];
         stack.push(arr[0]);
-        int count = 1;
+        int count = 0;
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < stack.peek() || arr[i] < max) {
+            if (arr[i] <= stack.peek() || arr[i] < max) {
                 stack.push(arr[i]);
             } else {
-                count++;
-                stack.clear();;
+                count += clearStack(stack);
                 stack.push(arr[i]);
                 max = arr[i];
             }
+        }
+        if (!stack.isEmpty()) {
+            count += clearStack(stack);
+        }
+        return count;
+    }
+
+    private int clearStack(Stack<Integer> stack) {
+        int top = stack.pop();
+        int count = 1;
+        while (!stack.isEmpty()) {
+            if (top != stack.pop()) {
+                stack.clear();
+                return 1;
+            }
+            count++;
         }
         return count;
     }
