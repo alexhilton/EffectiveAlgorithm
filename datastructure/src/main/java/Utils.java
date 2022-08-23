540711b1da60474d63d206fb688a56037b2efc82
Alex Hilton <dragonhilton@gmail.com>
2022-08-23 16:50:11 +0800

    Integer tricks.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

---
+++ datastructure/src/main/java/Utils.java
@@ -0,0 +1,16 @@
+public class Utils {
+    public static void integerTricks() {
+        int n = 10;
+        System.out.println(String.format("n is %d, x 2 = %d, / 2 = %d", n, n << 1, n >> 1));
+
+        System.out.println("Math pow(2, k to " + n + ")");
+        for (int k = 0; k <= n; k++) {
+            System.out.println(String.format("Math.pow(2, %d) = %d, 1 << %d = %d", k,
+                    (int) Math.pow(2, k), k, 1 << k));
+        }
+    }
+
+    public static void main(String[] args) {
+        integerTricks();
+    }
+}
