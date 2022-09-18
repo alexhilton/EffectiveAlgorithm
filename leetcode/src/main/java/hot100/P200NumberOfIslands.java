9da5dbcefe0f1a9b7c441832f5a51c0c8f8b0b64
Alex Hilton <dragonhilton@gmail.com>
2022-09-18 19:27:19 +0800

    P200 number of islands with disjoint set.
    Count in disjoint set.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/hot100/P200NumberOfIslands.java
+++ leetcode/src/main/java/hot100/P200NumberOfIslands.java
@@ -2,77 +2,91 @@ package hot100;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public class P200NumberOfIslands {
-    private Map<Integer, Integer> fathers;
-    private Map<Integer, Integer> rank;
+    private static int[][] DIRS = {
+            {-1, 0}, {0, -1}, {1, 0}, {0, 1}
+    };
 
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        fathers = new HashMap<>();
-        rank = new HashMap<>();
+        DisjointSet dset = new DisjointSet(grid);
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
-                    int key = i * m + j;
-                    System.out.println(" i " + i + ", j " + j + ", key -> " + key);
-                    if (!fathers.containsKey(key)) {
-                        fathers.put(key, key);
+                    int key = i * n + j;
+                    for (int[] d : DIRS) {
+                        int x = i + d[0];
+                        int y = j + d[1];
+                        if (x >= 0 && x < m && y >= 0 && y < n &&
+                                grid[x][y] == '1') {
+                            dset.union(key, x * n + y);
+                        }
                     }
-
-                    int topX = i - 1;
-                    boolean unionWithTop = topX >= 0 && grid[topX][j] == '1';
-                    int leftY = j - 1;
-                    boolean unionWithLeft = leftY >= 0 && grid[i][leftY] == '1';
-                    System.out.println("     withLeft " + unionWithLeft + ", with top " + unionWithTop);
-
-                    if (unionWithLeft && unionWithTop) {
-                        union(topX * m + j, key);
-                        union(topX * m + j, i * m + leftY);
-                    } else if (unionWithLeft) {
-                        union(i * m + leftY, key);
-                    } else if (unionWithTop) {
-                        union(topX * m + j, key);
-                    }
-
-                    System.out.println(fathers);
                 }
             }
         }
 
-        return (int) fathers.entrySet().stream()
-                .filter(entry -> Objects.equals(entry.getKey(), entry.getValue()))
-                .count();
+        return dset.getCount();
     }
 
-    private int find(int key) {
-        int f = fathers.get(key);
-        if (f == key) {
-            return f;
-        }
-        int ff = find(f);
-        fathers.put(key, ff);
-        return ff;
-    }
+    static class DisjointSet {
+        private Map<Integer, Integer> fathers;
+        private Map<Integer, Integer> rank;
+        private int count;
 
-    private void union(int u, int v) {
-        int fu = find(u);
-        int fv = find(v);
-        if (fu == fv) {
-            return;
+        public DisjointSet(char[][] data) {
+            final int m = data.length;
+            final int n = data[0].length;
+            fathers = new HashMap<>();
+            rank = new HashMap<>();
+            count = 0;
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (data[i][j] == '1') {
+                        int key = i * n + j;
+                        fathers.put(key, key);
+                        rank.put(key, 0);
+                        count++;
+                    }
+                }
+            }
         }
-        int ru = rank.getOrDefault(fu, 0);
-        int rv = rank.getOrDefault(fv, 0);
-        if (ru < rv) {
-            fathers.put(u, fv);
-        } else if (ru > rv) {
-            fathers.put(v, fu);
-        } else {
-            fathers.put(v, fu);
-            rank.put(fu, ru + 1);
+
+        private int find(int key) {
+            int f = fathers.get(key);
+            if (f == key) {
+                return f;
+            }
+            int ff = find(f);
+            fathers.put(key, ff);
+            return ff;
+        }
+
+        private void union(int u, int v) {
+            int fu = find(u);
+            int fv = find(v);
+            if (fu == fv) {
+                return;
+            }
+            int ru = rank.getOrDefault(fu, 0);
+            int rv = rank.getOrDefault(fv, 0);
+            if (ru < rv) {
+                fathers.put(u, fv);
+            } else if (ru > rv) {
+                fathers.put(v, fu);
+            } else {
+                fathers.put(v, fu);
+                rank.put(fu, ru + 1);
+            }
+            count--;
+        }
+
+        public int getCount() {
+            return count;
         }
     }
 }
