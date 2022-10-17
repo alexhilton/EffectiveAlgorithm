ec0e6b080a568873c5e5e4c3a8dcfdf2b3474105
Alex Hilton <dragonhilton@gmail.com>
2022-10-17 17:40:49 +0800

    P904 total fruits AC.
    Double pointers no real queue needed.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/daily/P904TotalFruits.java
+++ leetcode/src/main/java/daily/P904TotalFruits.java
@@ -6,25 +6,23 @@ import java.util.Queue;
 public class P904TotalFruits {
     public int totalFruit(int[] fruits) {
         int[] basket = {fruits[0], -1};
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(fruits[0]);
+        int left = 0;
         int result = 1;
-        for (int i = 1; i < fruits.length; i++) {
-            if (basket[1] == -1 || basket[0] == fruits[i] || basket[1] == fruits[i]) {
-                if (basket[1] == -1 && basket[0] != fruits[i]) {
-                    basket[1] = fruits[i];
+        for (int right = 1; right < fruits.length; right++) {
+            if (basket[1] == -1 || basket[0] == fruits[right] || basket[1] == fruits[right]) {
+                if (basket[1] == -1 && basket[0] != fruits[right]) {
+                    basket[1] = fruits[right];
                 }
-                queue.offer(fruits[i]);
             } else {
-                queue.clear();
-                basket[0] = fruits[i - 1];
-                basket[1] = fruits[i];
-                for (int j = i - 1; j >= 0 && fruits[j] == fruits[i - 1]; j--) {
-                    queue.offer(fruits[j]);
+                basket[0] = fruits[right - 1];
+                basket[1] = fruits[right];
+                int j = right - 1;
+                while (j >= left && fruits[j] == fruits[right - 1]) {
+                    j--;
                 }
-                queue.offer(fruits[i]);
+                left = j + 1;
             }
-            result = Math.max(queue.size(), result);
+            result = Math.max(right - left + 1, result);
         }
 
         return result;
