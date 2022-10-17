6035307934a08d82069c0332f7b337b3c4757aa2
Alex Hilton <dragonhilton@gmail.com>
2022-10-17 12:28:32 +0800

    P904 total fruits.
    Failed at test case 62 and testcase 59.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/daily/P904TotalFruits.java
+++ leetcode/src/main/java/daily/P904TotalFruits.java
@@ -15,9 +15,17 @@ public class P904TotalFruits {
                 } else if (basket[1] == fruits[i]) {
                     basketCount[1]++;
                 }
+                if (basket[0] == fruits[i]) {
+                    swap(basket);
+                    swap(basketCount);
+                }
             } else {
+                if (i - 2 >= 0 && fruits[i - 2] != basket[0]) {
+                    basketCount[0] = 1;
+                } else {
+                    basketCount[0] = basketCount[1];
+                }
                 basket[0] = basket[1];
-                basketCount[0] = basketCount[1];
                 if (basket[0] == fruits[i]) {
                     basketCount[0]++;
                     basket[1] = -1;
@@ -31,5 +39,11 @@ public class P904TotalFruits {
         }
 
         return result;
+    }
+
+    private static void swap(int[] a) {
+        int t = a[1];
+        a[1] = a[0];
+        a[0] = t;
     }
 }
