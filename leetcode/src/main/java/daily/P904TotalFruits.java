aa6321004e2ef12be4e387c11de0a0392bdf5e5c
Alex Hilton <dragonhilton@gmail.com>
2022-10-17 15:35:48 +0800

    P904 total fruits AC.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/daily/P904TotalFruits.java
+++ leetcode/src/main/java/daily/P904TotalFruits.java
@@ -1,49 +1,32 @@
 package daily;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class P904TotalFruits {
     public int totalFruit(int[] fruits) {
         int[] basket = {fruits[0], -1};
-        int[] basketCount = {1, 0};
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(fruits[0]);
         int result = 1;
         for (int i = 1; i < fruits.length; i++) {
             if (basket[1] == -1 || basket[0] == fruits[i] || basket[1] == fruits[i]) {
                 if (basket[1] == -1 && basket[0] != fruits[i]) {
                     basket[1] = fruits[i];
                 }
-                if (basket[0] == fruits[i]) {
-                    basketCount[0]++;
-                } else if (basket[1] == fruits[i]) {
-                    basketCount[1]++;
-                }
-                if (basket[0] == fruits[i]) {
-                    swap(basket);
-                    swap(basketCount);
-                }
+                queue.offer(fruits[i]);
             } else {
-                if (i - 2 >= 0 && fruits[i - 2] != basket[0]) {
-                    basketCount[0] = 1;
-                } else {
-                    basketCount[0] = basketCount[1];
+                queue.clear();
+                basket[0] = fruits[i - 1];
+                basket[1] = fruits[i];
+                for (int j = i - 1; j >= 0 && fruits[j] == fruits[i - 1]; j--) {
+                    queue.offer(fruits[j]);
                 }
-                basket[0] = basket[1];
-                if (basket[0] == fruits[i]) {
-                    basketCount[0]++;
-                    basket[1] = -1;
-                    basketCount[1] = 0;
-                } else {
-                    basket[1] = fruits[i];
-                    basketCount[1] = 1;
-                }
+                queue.offer(fruits[i]);
             }
-            result = Math.max(basketCount[0] + basketCount[1], result);
+            result = Math.max(queue.size(), result);
         }
 
         return result;
-    }
-
-    private static void swap(int[] a) {
-        int t = a[1];
-        a[1] = a[0];
-        a[0] = t;
     }
 }
