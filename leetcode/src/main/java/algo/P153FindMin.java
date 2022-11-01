15440a8b634eb69b66c74e6cbf5662cf902e7e63
Alex Hilton <dragonhilton@gmail.com>
2022-11-01 15:29:57 +0800

    P153 basics passed.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

---
+++ leetcode/src/main/java/algo/P153FindMin.java
@@ -0,0 +1,25 @@
+package algo;
+
+public class P153FindMin {
+    public int findMin(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + ((end - start) >> 1);
+            int left = mid == 0 ? nums.length - 1 : mid - 1;
+            int right = mid == nums.length - 1 ? 0 : mid + 1;
+            if (nums[mid] < nums[left] && nums[mid] < nums[right]) {
+                return nums[mid];
+            } else if (nums[mid] > nums[left] && nums[mid] < nums[right]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1; // should not be here.
+    }
+}
---
+++ leetcode/src/test/java/algo/P153FindMinTest.java
@@ -0,0 +1,36 @@
+package algo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class P153FindMinTest {
+    private P153FindMin instance;
+
+    @BeforeEach
+    public void setup() {
+        instance = new P153FindMin();
+    }
+
+    @Test
+    public void testDemos() {
+        assertEquals(1, instance.findMin(new int[] {3, 4, 5, 1, 2}));
+        assertEquals(0, instance.findMin(new int[] {4,5,6,7,0,1,2}));
+        assertEquals(11, instance.findMin(new int[] {11,13,15,17}));
+    }
+
+    @Test
+    public void testBasics() {
+        assertEquals(1, instance.findMin(new int[] {1}));
+        assertEquals(1, instance.findMin(new int[] {1, 2}));
+        assertEquals(1, instance.findMin(new int[] {2, 1}));
+    }
+
+    @Test
+    public void testMore() {
+        assertEquals(0, instance.findMin(new int[] {0, 1, 2}));
+        assertEquals(0, instance.findMin(new int[] {2, 0, 1}));
+        assertEquals(0, instance.findMin(new int[] {1, 2, 0}));
+    }
+}
