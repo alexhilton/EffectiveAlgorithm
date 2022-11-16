a2763cc087dbe252296d4e7293ea010a5f2af627
Alex Hilton <dragonhilton@gmail.com>
2022-11-16 22:41:01 +0800

    P775 WA at case 168.
    Mono stack is wrong answer totally.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/main/java/daily/P775IdealPerm.java
+++ leetcode/src/main/java/daily/P775IdealPerm.java
@@ -8,16 +8,18 @@ public class P775IdealPerm {
         int local = 0;
         int global = 0;
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < n; i++) {
+        for (int i = n - 1; i >= 0; i--) {
             if (i < n - 1 && nums[i] > nums[i + 1]) {
                 local++;
             }
-            while (!stack.isEmpty() && stack.peek() > nums[i]) {
+            while (!stack.isEmpty() && stack.peek() < nums[i]) {
                 global++;
                 stack.pop();
             }
             stack.push(nums[i]);
         }
+
+        System.out.println("local " + local + ", global " + global);
 
         return local == global;
     }
