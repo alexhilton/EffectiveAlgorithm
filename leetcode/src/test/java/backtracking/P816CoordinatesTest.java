cff08716738bbaf929e915d27c8decd142699442
Alex Hilton <dragonhilton@gmail.com>
2022-12-17 14:57:24 +0800

    P816 AC.
    Remove unused imports.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/test/java/backtracking/P816CoordinatesTest.java
+++ leetcode/src/test/java/backtracking/P816CoordinatesTest.java
@@ -1,13 +1,11 @@
 package backtracking;
 
-import jdk.jfr.Experimental;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 public class P816CoordinatesTest {
