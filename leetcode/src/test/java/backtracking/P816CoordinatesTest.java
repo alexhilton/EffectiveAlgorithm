edf4298f90a4bb6337c2ea1cf3fbcc7c5b8d19f5
Alex Hilton <dragonhilton@gmail.com>
2022-12-17 13:59:14 +0800

    P816 WA at testcase 200.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- leetcode/src/test/java/backtracking/P816CoordinatesTest.java
+++ leetcode/src/test/java/backtracking/P816CoordinatesTest.java
@@ -1,7 +1,14 @@
 package backtracking;
 
+import jdk.jfr.Experimental;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 public class P816CoordinatesTest {
     private P816Coordinates instance;
@@ -13,9 +20,53 @@ public class P816CoordinatesTest {
 
     @Test
     public void testDemos() {
-        System.out.println("123 -> " + instance.ambiguousCoordinates("(123)"));
-        System.out.println("00011 -> " + instance.ambiguousCoordinates("(00011)"));
-        System.out.println("0123 -> " + instance.ambiguousCoordinates("(0123)"));
-        System.out.println("100 -> " + instance.ambiguousCoordinates("(100)"));
+        doTest(List.of(
+                "(1, 23)", "(12, 3)", "(1.2, 3)", "(1, 2.3)"
+        ), "(123)");
+        doTest(List.of(
+                "(0.001, 1)", "(0, 0.011)"
+        ), "(00011)");
+        doTest(List.of(
+                "(0, 123)", "(0, 12.3)", "(0, 1.23)", "(0.1, 23)", "(0.1, 2.3)", "(0.12, 3)"
+        ), "(0123)");
+        doTest(List.of(
+                "(10, 0)"
+        ), "(100)");
+    }
+
+    @Test
+    public void testCase200() {
+        doTest(List.of(
+                "(0, 1.01)","(0, 10.1)","(0, 101)","(0.1, 0.1)"
+        ), "(0101)");
+    }
+
+    private void doTest(List<String> expected, String payload) {
+        List<String> exp = new ArrayList<>(expected);
+        exp.sort(this::compare);
+        List<String> actual = instance.ambiguousCoordinates(payload);
+        actual.sort(this::compare);
+        assertIterableEquals(exp, actual);
+    }
+
+    private int compare(String a, String b) {
+        int idxa = a.indexOf(", ");
+        int idxb = b.indexOf(", ");
+        int ret = compareC(a.substring(1, idxa), b.substring(1, idxb));
+        if (ret == 0) {
+            return compareC(a.substring(idxa + 2, a.length() - 1), b.substring(idxb + 2, b.length() - 1));
+        }
+        return ret;
+    }
+
+    private int compareC(String a, String b) {
+        float ret = Float.valueOf(a) - Float.valueOf(b);
+        if (ret == 0) {
+            return a.compareTo(b);
+        }
+        if (ret > 0) {
+            return 1;
+        }
+        return -1;
     }
 }
