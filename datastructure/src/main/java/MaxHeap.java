782a06d82d8561fca714d0ff94c481807c1ed467
Alex Hilton <dragonhilton@gmail.com>
2023-07-12 15:23:08 +0800

    Implement MaxHeap.
    Add comemnts.
    
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- datastructure/src/main/java/MaxHeap.java
+++ datastructure/src/main/java/MaxHeap.java
@@ -1,12 +1,19 @@
 
 /**
  * A bound Max Heap of int type elements.
+ * This is a binary heap with array as the underlying container.
  */
 public class MaxHeap {
     public static final int INF = Integer.MAX_VALUE;
 
     private final int capacity;
     private int size;
+    /*
+     * Put the elements into an array, but the logical relationship is a binary tree.
+     * 0 is the root;
+     * i's left child is 2*i + 1, right child is 2*i + 2;
+     * i's parent is (i-1) / 2.
+     */
     private final int[] elements;
 
     public MaxHeap(int capacity) {
@@ -16,13 +23,17 @@ public class MaxHeap {
     }
 
     /**
-     * Nothing happens if heap is full
+     * Nothing happens if heap is full.
      */
     public void offer(int e) {
         if (isFull()) {
             // Overflowed.
             return;
         }
+        /*
+         * Put the new element at the end of the heap.
+         * Push it up until it is less than its parent.
+         */
         size++;
         int i = size - 1;
         elements[i] = e;
@@ -57,6 +68,10 @@ public class MaxHeap {
             return elements[0];
         }
 
+        /*
+         * Root is the max value in the heap, will remove and return it to caller.
+         * Push down the tree and select the max of left and right as the new parent.
+         */
         int root = elements[0];
         elements[0] = elements[size - 1];
         size--;
@@ -72,6 +87,13 @@ public class MaxHeap {
         return size == capacity;
     }
 
+    /*
+     * Heapify the sub-tree rooted with index i.
+     * Find the largest value of parent, left and right;
+     * If the parent is the largest, we are done.
+     * Swap parent with the largest node, now parent is the largest;
+     * Keep heapifying the swapped sub-tree.
+     */
     private void heapify(int i) {
         int l = left(i);
         int r = right(i);
