60ba77a3f670d8a96b07b2879c5f7e42ff857e7c
Alex Hilton <dragonhilton@gmail.com>
2023-07-13 20:27:48 +0800

    Rolling hash.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- datastructure/src/main/java/Hashing.java
+++ datastructure/src/main/java/Hashing.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
 public class Hashing {
     public static final int MOD = 1_000_000_000 + 7;
 
@@ -21,8 +24,40 @@ public class Hashing {
         return hash;
     }
 
+    public static ArrayList<Integer> rollingHash(String payload, int window, int p, int mod) {
+        final int n = payload.length();
+        ArrayList<Integer> hashValues = new ArrayList<>(n - window + 1);
+        int power = 1;
+        for (int i = 0; i < window - 1; i++) {
+            power *= p;
+            power %= mod;
+        }
+
+        int hash = 0;
+        for (int i = 0; i < window; i++) {
+            hash = (hash * p + payload.charAt(i)) % mod;
+        }
+        hashValues.add(hash);
+
+        for (int i = 1; i < n - window + 1; i++) {
+            hash = (hash - power * payload.charAt(i - 1)) % mod;
+            hash = (hash * p + payload.charAt(i + window - 1)) % mod;
+            hashValues.add(hash);
+        }
+
+        return hashValues;
+    }
+
     public static void main(String[] args) {
         String s = "Hello, world of Hashing!";
         System.out.println(s + ", hash -> " + hash(s));
+
+        String payload = "abcabcabc";
+        int window = 3;
+        ArrayList<Integer> hashes = rollingHash(payload, window, 31, MOD);
+        System.out.println("Rolling hash of " + payload + ", window size " + window);
+        IntStream.range(0, hashes.size())
+                .mapToObj(i -> i + "->" + payload.substring(i, i + window) + " whose hash is " + hashes.get(i))
+                .forEach(System.out::println);
     }
 }
